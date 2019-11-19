1076e90fb8a918d68f1e6de2703dfc4489bf5325
Ujjwal Aggarwal <31439768+ujjwal123aggarwal@users.noreply.github.com>
2019-11-19 09:17:14 -0800

    Add files via upload

---
+++ AR_3d_drawing/Assets/painting.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class painting : MonoBehaviour {
+    public GameObject bluepaint;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(2))
+        {
+            Instantiate(bluepaint, transform.position, transform.rotation);
+        }
+	}
+}
